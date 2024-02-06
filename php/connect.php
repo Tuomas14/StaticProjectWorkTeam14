9b88ecab978fcd0ad7624e3505940b2abde829db
koskinenj <jani23006@student.hamk.fi>
2024-02-06 09:47:49 +0200

    .

---
+++ php/connect.php
@@ -0,0 +1,14 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$init=parse_ini_file("./.ht.asetukset.ini");
+
+try{
+    $yhteys=mysqli_connect($init["palvelin"], $init ["tunnus"], $init["pass"], $init["tk"]);
+}
+catch(Exception $e){
+    header("Location:../html/yhteysvirhe.html");
+    exit;
+}
+
+?>
