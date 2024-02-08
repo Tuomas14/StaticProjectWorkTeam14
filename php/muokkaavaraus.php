33fd2e737dc51819336f5743d0ab01f90374aeff
Aapo50 <aapo.leppikallio@student.hamk.fi>
2024-02-08 11:14:01 +0200

    .

--- php/muokkaavaraus.php
+++ php/muokkaavaraus.php
@@ -1,7 +1,7 @@
 <?php
 include ("./connect.php");
 
-$muokattava=isset($_GET["muokattava"]) ? $_GET["muokattava"] : "";
+$muokattava=isset($_GET["varaustunnus"]) ? $_GET["varaustunnus"] : "";
 
 //Jos tietoa ei ole annettu, palataan listaukseen
 if (empty($muokattava)){
@@ -9,10 +9,10 @@ if (empty($muokattava)){
     exit;
 }
 
-$sql="select * from ASIAKAS where asiakasID=?";
+$sql="select * from ASIAKAS where varaustunnus=?";
 $stmt=mysqli_prepare($yhteys, $sql);
 //Sijoitetaan muuttuja sql-lauseeseen
-mysqli_stmt_bind_param($stmt, 'i', $muokattava);
+mysqli_stmt_bind_param($stmt, 's', $muokattava);
 //Suoritetaan sql-lause
 mysqli_stmt_execute($stmt);
 //Koska luetaan prepared statementilla, tulos haetaan 
