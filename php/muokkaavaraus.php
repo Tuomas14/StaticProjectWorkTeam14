8456b9f990f031d1d2d0a46ec0d132eb11594547
Tuomas <tuomas.valtonen@student.hamk.fi>
2024-02-08 11:42:40 +0200

    Muokkaavaraus.php and muokkaavaraus.html
    Muokkaavaraus.php:hen poista ominaisuus. Muokkaavaraus.html:lään yhdistys .php koodiin.

--- php/muokkaavaraus.php
+++ php/muokkaavaraus.php
@@ -1,7 +1,23 @@
 <?php
 include ("./connect.php");
 
+// Tehdään funktio varauksen poistamiseen
+function poistaVaraus($yhteys, $poistettava) {
+    $sql="DELETE FROM varaukset WHERE id=?";
+    $stmt=mysqli_prepare($yhteys, $sql);
+    // Sijoitetaan muuttuja SQL-lauseeseen
+    mysqli_stmt_bind_param($stmt,"i", $poistettava);
+    // Suoritetaan SQL-lause
+    mysqli_stmt_execute($stmt);
+    if(mysqli_stmt_affected_rows($stmt)> 0) {
+    print "Varaus on poistettu onnistuneesti!";
+    } else {
+        print "Virhe: Varausta ei voitu poistaa.";
+}
+}
+
 $muokattava=isset($_GET["muokattava"]) ? $_GET["muokattava"] : "";
+$poistettava=isset($_GET["poistettava"]) ? $_GET["poistettava"] : "";
 
 //Jos tietoa ei ole annettu, palataan listaukseen
 if (empty($muokattava)){
@@ -35,8 +51,20 @@ sukunimi:<input type='text' name='sukunimi' value='<?php print $rivi->sukunimi;?
 <input type='submit' name='ok' value='ok'><br>
 </form>
 
-<!-- loppuun uusi php-osuus -->
 <?php
-//Suljetaan tietokantayhteys
+
+// Jos tieto on annettu, suoritetaan poisto tietokannasta
+if (!empty($poistettava)) {
+    // Kutsutaan funktiota varauksen poistamiseen
+    poistaVaraus($yhteys, $poistettava);    
+} else {
+    //Jos varaustunnusta ei ole määritelty, palautetaan virheviesti
+    print "Virhe: Varaustunnusta ei ole määritelty.";
+}
+
 mysqli_close($yhteys);
+
+// Ohjataan takaisin index.html listaukseen
+header("Location:../index.html");
+exit;
 ?>
