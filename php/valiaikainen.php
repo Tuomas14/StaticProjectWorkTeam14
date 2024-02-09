e6f260f55e992df4d82d088cc40cf05b99c7fb13
Tuomas <tuomas.valtonen@student.hamk.fi>
2024-02-09 11:00:30 +0200

    Update valiaikainen.php

--- php/valiaikainen.php
+++ php/valiaikainen.php
@@ -29,4 +29,24 @@ if ($stmt->execute()) {
 
 $yhteys->close();
 
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Varauksen muokkaus</title>
+</head>
+<body>
+    <h2>Syötä varaustunnus varauksen tietojen muokkaamiseksi</h2>
+    <form method="post" action="">
+        Varaustunnus: <input type="text" name="varaustunnus" maxlength="5"><br><br>
+        <input type="submit" value="Hae ja muokkaa varauksen tietoja">
+    </form>
+    <!-- Lisätään poistanappi -->
+    <form method="post" action="">
+        <input type="hidden" name="poistettava" value="<?php echo isset($_POST['varaustunnus']) ? $_POST['varaustunnus'] : ''; ?>">
+        <input type="submit" name="poista" value="Poista varaus">
+    </form>
+</body>
+</html>
